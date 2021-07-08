aada6d740bf74a6757ae6ba5c85804988b4be037
Evan Simon <evansimon2001@gmail.com>
2021-07-08 00:02:15 -0500

    Added requirements

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,4 +1,8 @@
-# hello
-class FancyGitAdd():
-    def main():
+from plumbum import cli
+
+class FancyGitAdd(cli.Application):
+    def main(self):
         print("Welcome to Fancy Git Add!")
+
+if __name__ == "__main__":
+    FancyGitAdd()
