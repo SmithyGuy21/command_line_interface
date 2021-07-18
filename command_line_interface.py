396ea2d45b58d570403a9459ceec100190f57040
Evan Simon <evansimon2001@gmail.com>
2021-07-18 01:21:28 -0500

    import plumbum.cmd not working

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,16 +1,28 @@
 from pyfiglet import Figlet
 from plumbum import cli
+from plumbum.cmd import ls
 
 def print_banner(text):
     print(Figlet(font="slant").renderText(text))
 
 
 def get_files():
-    return []   # incomplete function
+    ls_output = ls().strip()
+    files = ls_output.split("\n")
+    return files   
+
+
+def test_get_files():
+    files = get_files()
+    assert len(files) == 5, "There should be 5 files"
+
+
 class FancyGitAdd(cli.Application):
     VERSION = "1.1"
     def main(self):
-        print("Welcome to Fancy Git Add!")
+        print_banner("Welcome to Fancy Git Add!")
 
 if __name__ == "__main__":
     FancyGitAdd()
+    get_files()
+
