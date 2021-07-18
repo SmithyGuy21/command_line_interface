8410e30015c6a635290fd680a17d4ed4c94a1bc3
Evan Simon <evansimon2001@gmail.com>
2021-07-18 00:27:19 -0500

    Added functions

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,5 +1,12 @@
+from pyfiglet import Figlet
 from plumbum import cli
 
+def print_banner(text):
+    print(Figlet(font="slant").renderText(text))
+
+
+def get_files():
+    return []   # incomplete function
 class FancyGitAdd(cli.Application):
     VERSION = "1.1"
     def main(self):
